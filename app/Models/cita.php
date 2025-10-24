589e14a0637fe38d4ef17ecde5a2246436ee7933
JosePecho <pechojose22@gmail.com>
2025-10-24 14:03:59 -0500

    modifique el codigo del modelo cita

--- app/Models/cita.php
+++ app/Models/cita.php
@@ -2,9 +2,29 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class cita extends Model
+class Cita extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'dni_estudiante',
+        'fecha_cita',
+        'motivo',
+        'estado',
+        'correo_enviado',
+    ];
+
+    protected $casts = [
+        'fecha_cita' => 'datetime',
+        'correo_enviado' => 'boolean',
+    ];
+
+    // Relación con Estudiante
+    public function estudiante()
+    {
+        return $this->belongsTo(Estudiante::class, 'dni_estudiante', 'dni');
+    }
 }
