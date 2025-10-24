00abc17531b0464d28fcd0d310420637a7df8932
josue18K <josuern5593@gmail.com>
2025-10-24 14:28:20 -0500

    Correcciones generales de los archivos migrations, models, y factorys

---
+++ app/Models/Apoderado.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Apoderado extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'nombre',
+        'parentesco',
+        'correo',
+        'telefono',
+    ];
+
+    // Relación: Un apoderado puede tener muchos estudiantes
+    public function estudiantes()
+    {
+        return $this->hasMany(Estudiante::class, 'apoderado_id');
+    }
+}
