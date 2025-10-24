10b69b043d9d561bcbe665d770b27686cce17331
rosario142 <rosarioatanacioheydy02@gmail.com>
2025-10-24 13:13:00 -0600

    agrege cod en models notificaciones y reporte

--- app/Models/Reporte.php
+++ app/Models/Reporte.php
@@ -2,9 +2,24 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Reporte extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'psicologo_id',
+        'mes',
+        'total_estudiantes',
+        'casos_criticos',
+        'observaciones',
+    ];
+
+    // Relación con Psicólogo (User)
+    public function psicologo()
+    {
+        return $this->belongsTo(User::class, 'psicologo_id');
+    }
 }
