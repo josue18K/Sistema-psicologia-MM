b4488f05455fdbd0962ba7ab984e410154bad17a
rosario142 <rosarioatanacioheydy02@gmail.com>
2025-10-24 11:48:31 -0600

    columna para citas y reporte

--- database/migrations/2025_10_24_165507_create_citas_table.php
+++ database/migrations/2025_10_24_165507_create_citas_table.php
@@ -6,20 +6,21 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('citas', function (Blueprint $table) {
             $table->id();
+            $table->string('dni_estudiante', 8);
+            $table->dateTime('fecha_cita');
+            $table->text('motivo');
+            $table->enum('estado', ['PENDIENTE', 'REALIZADA', 'CANCELADA'])->default('PENDIENTE');
+            $table->boolean('correo_enviado')->default(false);
             $table->timestamps();
+
+            $table->foreign('dni_estudiante')->references('dni')->on('estudiantes')->onDelete('cascade');
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('citas');
