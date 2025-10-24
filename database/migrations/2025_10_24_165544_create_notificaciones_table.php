85163122ae8439fd6c172920358592c677d52a81
JosePecho <pechojose22@gmail.com>
2025-10-24 12:28:17 -0500

    modifique el codigo

--- database/migrations/2025_10_24_165544_create_notificaciones_table.php
+++ database/migrations/2025_10_24_165544_create_notificaciones_table.php
@@ -6,20 +6,19 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('notificaciones', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('remitente_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('destinatario_id')->constrained('users')->onDelete('cascade');
+            $table->text('mensaje');
+            $table->dateTime('fecha_envio');
+            $table->boolean('leida')->default(false);
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('notificaciones');
